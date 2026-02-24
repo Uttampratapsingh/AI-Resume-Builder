3d074263b836e20a397e1d9c34d6359da3ceea56
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-02-24 15:42:16 +0530

    Implemented footer

--- Frontend/src/components/home/CallToAction.jsx
+++ Frontend/src/components/home/CallToAction.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CallToAction = () => {
   return (
-    <div>CallToAction</div>
-  )
+        <>
+            <style>{`
+                @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+            
+                * {
+                    font-family: 'Poppins', sans-serif;
+                }
+            `}</style>
+            <div id="cta" className="max-w-5xl py-16 md:pl-20 md:w-full max-md:text-center mx-2 md:mx-auto flex flex-col md:flex-row items-center justify-between text-left bg-gradient-to-b from-[#008000] to-green rounded-2xl p-10 text-white">
+                <div>
+                    <h1
+                        className="text-4xl md:text-[46px] md:leading-[60px] font-semibold bg-gradient-to-r from-white to-white text-transparent bg-clip-text">
+                        Ready to try-out this tool?
+                    </h1>
+                    <p className="bg-gradient-to-r from-white to-white text-transparent bg-clip-text text-lg">
+                        Your next favourite tool is just one click away.
+                    </p>
+                </div>
+                <Link to='login' className="px-12 py-3 text-slate-800 bg-white rounded-full text-sm mt-4">
+                    Get Started
+                </Link>
+            </div>
+        </>
+    );
 }
 
 export default CallToAction
--- Frontend/src/components/home/Testimonials.jsx
+++ Frontend/src/components/home/Testimonials.jsx
@@ -4,7 +4,7 @@ import Title from './Title'
 
 const Testimonials = () => {
   return (
-    <div id='features' className='flex flex-col items-center my-10 scroll-mt-12'>
+    <div id='testimonials' className='flex flex-col items-center my-10 scroll-mt-12'>
 
             <div className="flex items-center align-middle gap-2 text-sm text-green-800 bg-green-400/10 rounded-full px-6 py-1.5">
                 <BookUserIcon className='size-4.5 stroke-green-600' />
