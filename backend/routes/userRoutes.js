566487c84b1c183e9b0b3eeea685fed62d8cd6bd
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-02-25 20:36:25 +0530

    Integrate the routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { getUserById, loginUser, registerUser } from '../controllers/userController.js';
+import protect from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/data', protect, getUserById);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import router from './routes/userRoutes.js';
 
 dotenv.config(); // Load environment variables from .env file
 await connectDB(); // Connect to the database before starting the server
@@ -15,6 +16,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello from the backend!');
 });
+
+app.use('/api/users',router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
