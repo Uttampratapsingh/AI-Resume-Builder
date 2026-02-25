040c9c53912565bea2a4a806ae1586f1e73a2897
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-02-25 18:55:18 +0530

    Sucessfully connected to mongodb server

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config(); // Load environment variables from .env file
+await connectDB(); // Connect to the database before starting the server
+
+const app = express();
+const PORT = process.env.PORT || 3000;  
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from the backend!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
