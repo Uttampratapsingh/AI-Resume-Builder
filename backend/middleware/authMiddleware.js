0dafcef008c840a6f40b24d5004353ca919a0f09
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-02-25 20:28:39 +0530

    added the middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+//
+const protect = async (req, res, next)=>{
+    const token = req.headers.authorization; // Get the token from the Authorization header
+    if(!token){
+        return res.status(401).json({message: "Not authorized, no token"});
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verify the token using the secret key. If the token is valid, it will return the decoded payload, which contains the userId that we included when we generated the token during registration or login.
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        return res.status(401).json({message: "Not authorized, token failed"});
+    }
+}
+
+export default protect;
