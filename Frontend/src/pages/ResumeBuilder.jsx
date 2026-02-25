6b3ee8c4b70a44d178134696bcac2ebaa5435a38
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-02-25 14:52:24 +0530

    Mounted the summary component in resume builder

--- Frontend/src/pages/ResumeBuilder.jsx
+++ Frontend/src/pages/ResumeBuilder.jsx
@@ -7,6 +7,7 @@ import PersonalInfoForm from '../components/PersonalInfoForm';
 import ResumePreview from '../components/ResumePreview';
 import TemplateSelector from '../components/TemplateSelector';
 import ColorPicker from '../components/ColorPicker';
+import ProfessionalSummary from '../components/ProfessionalSummary';
 
 const ResumeBuilder = () => {
 
@@ -129,7 +130,10 @@ const ResumeBuilder = () => {
               <div className='space-y-6'>
                 {activeSection.id === 'personal' && (
                   <PersonalInfoForm data={resumeData.personal_info} onChange={(data)=>setResumeData(prev=>({...prev,personal_info:data}))} removeBackground={removeBackground} setRemoveBackground={setRemoveBackground} />
-                )} 
+                )}
+                {activeSection.id === 'summary' && (
+                  <ProfessionalSummary data={resumeData.professional_summary} onChange={(data)=>setResumeData(prev=>({...prev,professional_summary:data}))} setResumeData={setResumeData} />
+                )}
               </div>
               
             </div>
