04adaad0963a36c137e66c39b149473fb2c89312
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-02-26 11:02:23 +0530

    implented proivder in main & conditional rendering for the layout

--- Frontend/src/pages/Layout.jsx
+++ Frontend/src/pages/Layout.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import Loader from '../components/Loader';
+import Login from './Login';
+import { useSelector } from 'react-redux';
 
 const Layout = () => {
+
+  const {user,loading} = useSelector(state => state.auth); // here we are using the useSelector hook to get the user and loading state from the auth slice of the redux store. this will allow us to conditionally render the navbar based on whether the user is logged in or not.
+
+  if(loading){
+    return <Loader/>;
+  }
+
   return (
-    <>
-    <div className='min-h-screen bg-gray-50'>
-      <Navbar/>
-      <Outlet/>
-    </div>
+    <> 
+    {
+      user ? 
+      <div className='min-h-screen bg-gray-50'>
+        <Navbar/>
+        <Outlet/>
+      </div>
+      :
+      <Login/>
+    }
+    
     </>
   )
 }
