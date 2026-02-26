1c4b4aa24fc8a7cffabacd768b15cffd66db5bf5
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-02-26 09:36:30 +0530

    Intialise the environmetal variable and OpenAI

--- backend/routes/resumeRoutes.js
+++ backend/routes/resumeRoutes.js
@@ -9,7 +9,7 @@ const resumeRouter = express.Router();
 
 
 resumeRouter.post('/create',protect,createResume);
-resumeRouter.put('/update',upload.single('image'),protect,updateResume);
+resumeRouter.put('/update',upload.single('image'),protect,updateResume); // Update route with multer middleware to handle image upload. it will look for an 'image' field in the request body and process the file upload before calling the updateResume controller.
 resumeRouter.delete('/delete/:resumeId',protect,deleteResume);
 resumeRouter.get('/get/:resumeId',protect,getResumeById);
 resumeRouter.get('/public/:resumeId',getPublicResumeById);
